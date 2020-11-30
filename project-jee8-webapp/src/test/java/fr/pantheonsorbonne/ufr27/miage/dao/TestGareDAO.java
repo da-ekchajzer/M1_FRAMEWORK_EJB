a7848d8fc86b11aeb2ad50b189734cfba42d2d7c
da-ekchajzer <da.ekchajzer@gmail.com>
2020-11-30 11:46:13 +0100

    test inject

--- project-jee8-webapp/src/test/java/fr/pantheonsorbonne/ufr27/miage/dao/TestGareDAO.java
+++ project-jee8-webapp/src/test/java/fr/pantheonsorbonne/ufr27/miage/dao/TestGareDAO.java
@@ -5,11 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import fr.pantheonsorbonne.ufr27.miage.n_dao.GareDAO;
 import fr.pantheonsorbonne.ufr27.miage.n_jpa.Gare;
@@ -17,28 +20,28 @@ import fr.pantheonsorbonne.ufr27.miage.tests.utils.TestUtils;
 
 public class TestGareDAO {
 
-	//static EntityManager em;
+	@Inject
+	static EntityManager em;
+	@Inject
 	static GareDAO gareDAO;
 
-//	@BeforeClass
-//	public static void setup() {
-//		em = TestUtils.startServer();
-//		em.getTransaction().begin();
-//
-//		String[] nomGares = { "Paris - Gare de Lyon", "Avignon-Centre", "Aix en Provence", "Marseille - St Charles",
-//				"Dijon-Ville", "Lyon - Pardieu", "Narbonne", "Sete", "Perpignan", "Paris - Montparnasse", "Tours",
-//				"Bordeaux - Saint-Jean", "Pessac", "Arcachon-Centre", "Nantes" };
-//
-//		Map<String, Gare> gares = new HashMap<>();
-//		for (String nomGare : nomGares) {
-//			Gare g = new Gare(nomGare);
-//			gares.put(nomGare, g);
-//			em.persist(g);
-//		}
-//		em.getTransaction().commit();
-//		
-//		gareDAO = new GareDAO();
-//	}
+	@BeforeClass
+	public static void setup() {
+		TestUtils.startServer();
+		em.getTransaction().begin();
+
+		String[] nomGares = { "Paris - Gare de Lyon", "Avignon-Centre", "Aix en Provence", "Marseille - St Charles",
+				"Dijon-Ville", "Lyon - Pardieu", "Narbonne", "Sete", "Perpignan", "Paris - Montparnasse", "Tours",
+				"Bordeaux - Saint-Jean", "Pessac", "Arcachon-Centre", "Nantes" };
+
+		Map<String, Gare> gares = new HashMap<>();
+		for (String nomGare : nomGares) {
+			Gare g = new Gare(nomGare);
+			gares.put(nomGare, g);
+			em.persist(g);
+		}
+		em.getTransaction().commit();
+	}
 	
 	@Test
 	public void testGetGaresByNom() {
@@ -48,9 +51,9 @@ public class TestGareDAO {
 		assertEquals("Avignon-Centre", gares.get(0).getNom());
 	}
 	
-//	
-//	@AfterClass
-//	public static void end() {
-//		TestUtils.stopServer();
-//	}
+	
+	@AfterClass
+	public static void end() {
+		TestUtils.stopServer();
+	}
 }
